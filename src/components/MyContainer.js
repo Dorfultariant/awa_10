c4cda10ef0119e9aaa5b5a761d38b9d13fb4d16e
TH <tunxero@hotmail.com>
2024-07-12 21:01:49 +0300

    1 2 maybe 3

---
+++ src/components/MyContainer.js
@@ -0,0 +1,5 @@
+import "./MyList";
+
+
+
+export default MyContainer;
---
+++ src/components/MyList.js
@@ -0,0 +1,15 @@
+function MyList(header, items) {
+    return (
+        <div>
+            <h2>{header}</h2>
+            <ol>
+                {items.map((item) => {
+                    <li key={item.id}> {item.text}</li>
+                })}
+            </ol>
+        </div >
+    );
+}
+
+
+export default MyList;
