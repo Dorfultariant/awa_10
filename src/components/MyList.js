33507c3a3d9d49f392c8fe1e9381e4ecd3412c85
TH <tunxero@hotmail.com>
2024-07-13 07:07:18 +0300

    removed none from styling

--- src/components/MyList.js
+++ src/components/MyList.js
@@ -2,14 +2,13 @@ import { React } from "react";
 
 function MyList({ header, items, updateItem }) {
 
-
     return (
         // I guess its important to use () instead of {} ........
         <div>
             <h2>{header}</h2>
             <ol>
                 {items.map((item) => (
-                    <li key={item.id} onClick={() => updateItem(item.id)} style={{ textDecoration: item.clicked ? "line-through" : "none" }}> {item.text}</li>
+                    <li key={item.id} onClick={() => updateItem(item.id)} style={{ textDecoration: item.clicked ? "line-through" : "" }}> {item.text}</li>
                 ))}
             </ol>
         </div >
